0759fa1ffe28e02dd88fa29a15b072c121440b09
klothe-africa <klothe.africa@gmail.com>
2022-09-23 00:18:26 +0200

    Car Class OOP Practice

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+#*Problem Statement
+#Create a Car class with two instance attributes:
+
+#.color, which stores the name of the car’s color as a string
+#.mileage, which stores the number of miles on the car as an integer
+#Then instantiate two Car objects—a blue car with 20,000 miles and a red car with 30,000 miles—and print out their colors and mileage.
+
+
+class Car:
+    def __int__(self, color, mileage):
+
+
